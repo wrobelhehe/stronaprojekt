76b8007849e22126dbcb2b353164305da4fc600d
wrobelhehe <wiktorwrobel7331@gmail.com>
2021-01-07 17:45:52 +0100

    add html css script

---
+++ script.js
@@ -0,0 +1,17 @@
+const menuBtn = document.querySelector(".nav-menu-btn");
+const navMenuItems = document.querySelector(".nav-menu-items");
+const menuItem = document.querySelectorAll(".menu-item");
+menuBtn.addEventListener("click", ()=>{
+    menuBtn.classList.toggle("open");
+    navMenuItems.classList.toggle("open");
+});
+
+
+menuItem.forEach(item =>{
+    item.addEventListener("click", ()=>{
+        if(menuBtn.classList.contains("open")) {
+            menuBtn.classList.toggle("open");
+            navMenuItems.classList.toggle("open");
+        }
+    });
+});
